532d119e8f02eb82ed988dada4a04fa45b2f465f
Carson Clarke-Magrab <carsonclarke570@gmail.com>
2016-10-21 15:41:07 -0400

    -Finished implementation of DiscreteSet

--- src/com/automata/datastructures/DiscreteSet.java
+++ src/com/automata/datastructures/DiscreteSet.java
@@ -1,8 +1,8 @@
 package com.automata.datastructures;
 import java.util.AbstractSet;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Set;
 
 /**
  * A helper data structure that functions as a discrete math set
@@ -11,7 +11,7 @@ import java.util.Set;
  *
  * @param <E> Any object
  */
-public class DiscreteSet<E extends Comparable<E>> extends AbstractSet<E>{
+public class DiscreteSet<E> extends AbstractSet<E>{
 
 	private transient HashMap<E, Object> map;
 	private static final Object PRESENT = new Object();
@@ -21,6 +21,66 @@ public class DiscreteSet<E extends Comparable<E>> extends AbstractSet<E>{
 	 */
 	public DiscreteSet() {
 		map = new HashMap<>();
+	}
+	
+	public DiscreteSet(Collection<? extends E> c) {
+		this();
+		for (E e: c) {
+			add(e);
+		}
+	}
+	
+	/**
+	 * Performs the union operation. Doesn't alter state data, creates 
+	 * a new instance of a DiscreteSet
+	 * 
+	 * @param other The set to perform the union with
+	 * @return the newly created set
+	 */
+	public DiscreteSet<E> union(DiscreteSet<E> other) {
+		DiscreteSet<E> newset = new DiscreteSet<>();
+		newset.addAll(this);
+		newset.addAll(other);
+		return newset;
+		
+	}
+	
+	/**
+	 * Performs the intersect operation. Doesn't alter state date, creates
+	 * a new instance of a DiscreteSet
+	 * 
+	 * @param other  The set to intersect with
+	 * @return The newly created set
+	 */
+	public DiscreteSet<E> intersect(DiscreteSet<E> other) {
+		DiscreteSet<E> newset = new DiscreteSet<>();
+		Iterator<E> i = iterator();
+		while(i.hasNext()) {
+			E e = i.next();
+			if (other.contains(e)) {
+				newset.add(e);
+			}
+		}
+		return newset;
+	}
+	
+	public DiscreteSet<DiscreteSet<E>> powerSet() {
+		DiscreteSet<DiscreteSet<E>> ps = new DiscreteSet<>();
+		ps.add(new DiscreteSet<>());
+		
+		for (E e: this) {
+			DiscreteSet<DiscreteSet<E>> newPs = new DiscreteSet<>();
+			
+			for (DiscreteSet<E> subset: ps) {
+				newPs.add(subset);
+				
+				DiscreteSet<E> newSubset = new DiscreteSet<>(subset);
+				newSubset.add(e);
+				newPs.add(newSubset);
+			}
+			ps = newPs;
+		}
+		return ps;
 	}
 	
 	/**
